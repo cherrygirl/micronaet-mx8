d0b416b925f83fd01caacdb53daa77ce0e972b52
nicothebrush <nicola.riolini@gmail.com>
2015-10-20 20:54:41 +0200

    [ADD] Start report for manage textilene material

---
+++ textilene_status/__openerp__.py
@@ -0,0 +1,48 @@
+###############################################################################
+#
+#    Copyright (C) 2001-2014 Micronaet SRL (<http://www.micronaet.it>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+{
+    'name': 'Textilene status',
+    'version': '0.1',
+    'category': 'Report',
+    'description': """
+        Manage status report for textilene material
+        Use BOM only for this type of product
+        """,
+    'author': 'Micronaet S.r.l. - Nicola Riolini',
+    'website': 'http://www.micronaet.it',
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+        'report_webkit',
+        ],
+    'init_xml': [],
+    'demo': [],
+    'data': [
+        #'security/ir.model.access.csv',
+        
+        #'textilene_views.xml',
+        #'wizard/textilene_status_report.xml',
+
+        #'report/textilene_status_report.xml',
+        ],
+    'active': False,
+    'installable': True,
+    'auto_install': False,
+    }
