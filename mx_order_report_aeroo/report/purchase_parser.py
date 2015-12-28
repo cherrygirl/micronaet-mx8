52e88f781e1c4088a56f8dab5cb35ce0fff5d03d
cherrygirl <anna@micronaet.it>
2015-12-28 16:58:58 +0100

    Move sale report in purchase

---
+++ mx_order_report_aeroo/report/purchase_parser.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#   Copyright (C) 2010-2012 Associazione OpenERP Italia
+#   (<http://www.openerp-italia.org>).
+#   Copyright(c)2008-2010 SIA "KN dati".(http://kndati.lv) All Rights Reserved.
+#                   General contacts <info@kndati.lv>
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from openerp.report import report_sxw
+from openerp.report.report_sxw import rml_parse
+
+
+class Parser(report_sxw.rml_parse):
+    counters = {}
+    
+    def __init__(self, cr, uid, name, context):
+        
+        super(Parser, self).__init__(cr, uid, name, context)
+        self.localcontext.update({
+            'get_counter': self.get_counter,
+            'set_counter': self.set_counter,
+            'bank': self.get_company_bank,
+        })
+
+    def get_company_bank(self, obj, field):
+        ''' Short function for readability
+        '''
+        try:
+           return obj.bank_account_company_id.__getattr__(field)
+        except:
+            return ''   
+
+    def get_counter(self, name):
+        ''' Get counter with name passed (else create an empty)
+        '''
+        if name not in self.counters:
+            self.counters[name] = False
+        return self.counters[name]
+
+    def set_counter(self, name, value):
+        ''' Set counter with name with value passed
+        '''
+        self.counters[name] = value
+        return "" # empty so no write in module
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
